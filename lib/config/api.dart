e0f8bf42311cf8c7098f00e35212a90410b714ae
Taufik Nurrahman <official.taufik980@gmail.com>
2023-09-27 10:31:28 +0700

    update api

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const baseURl = "https://bwa-general.000webhostapp.com/";
+  static const baseURl = "https://bwa-general.000webhostapp.com";
 
   static const history = "$baseURl/history";
   static const user = "$baseURl/user";
